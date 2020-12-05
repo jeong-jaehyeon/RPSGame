31d61e9753595a6b430beaf6f2a3de09a7812940
Jeong Jae Hyeon <wolgus104@naver.com>
2020-12-05 23:47:03 +0900

    4번째작업
    texttospeech를 통한 음성인식,
    random함수를 이용한 처리.

--- app/src/main/java/com/example/rpsgame/MainActivity.java
+++ app/src/main/java/com/example/rpsgame/MainActivity.java
@@ -9,6 +9,9 @@ import android.support.v4.os.IResultReceiver;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Locale;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView handAnimationImage;
@@ -20,7 +23,14 @@ public class MainActivity extends AppCompatActivity {
     TextToSpeech.OnInitListener onInitListener = new TextToSpeech.OnInitListener() {
         @Override
         public void onInit(int i) {
-
+            if (i != TextToSpeech.ERROR) {
+                //음성의 언어
+                textToSpeech.setLanguage(Locale.KOREA);
+                //음성의 높낮이
+                textToSpeech.setPitch(1.0f);
+                //음성의 속도
+                textToSpeech.setSpeechRate(1.0f);
+            }
         }
     };
 
@@ -37,16 +47,15 @@ public class MainActivity extends AppCompatActivity {
         //VISIBLE = 보이게 하는 기능.
         //INVISIBLE = 있지만 보이게 하지 않게하는 기능.
 
-
+        // Activity main의 이미지뷰를 바꿈으로써 코드 삭제 처리.
 //        첫시작을 computer 이미지로 해야하니 GONE을 써줌.
-        handAnimationImage.setVisibility(View.GONE);
+//        handAnimationImage.setVisibility(View.GONE);
 //        명확하게 표현해줌.
-        computerAnimationImage.setVisibility(View.VISIBLE);
-
+//        computerAnimationImage.setVisibility(View.VISIBLE);
 
         animationDrawable = (AnimationDrawable) handAnimationImage.getDrawable();
+        textToSpeech = new TextToSpeech(getApplicationContext(), onInitListener);
 
-//        animationDrawable.start();
     }
 
 
@@ -57,24 +66,75 @@ public class MainActivity extends AppCompatActivity {
                 computerAnimationImage.setVisibility(View.GONE);
                 handAnimationImage.setVisibility(View.VISIBLE);
                 animationDrawable.start();
+                //https://developer.android.com/reference/android/speech/tts/TextToSpeech#speak(java.lang.CharSequence,%20int,%20android.os.Bundle,%20java.lang.String)
+                voicePlay("가위바위보");
                 break;
 
-//            case R.id.gaweButton:
-//
-//                break;
-//            case R.id.baweButton:
-//
-//                break;
-//            case R.id.boButton:
-//
-//                break;
-
-            default:
+            case R.id.gaweButton:
+            case R.id.baweButton:
+            case R.id.boButton:
                 animationDrawable.stop();
                 handAnimationImage.setVisibility(View.GONE);
                 computerAnimationImage.setVisibility(View.VISIBLE);
-                computerAnimationImage.setImageResource(R.drawable.com_gawe);
+                int getComHand = new Random().nextInt(3) + 1;
+
+                switch (getComHand) {
+                    case 1:
+                        computerAnimationImage.setImageResource(R.drawable.com_gawe);
+                        if (view.getId() == R.id.gaweButton) {
+                            voicePlay("무승부");
+
+                        } else if (view.getId() == R.id.boButton) {
+                            voicePlay("컴퓨터 승리");
+
+                        } else {
+                            voicePlay("유저 승리");
+
+                        }
+                        break;
+                    case 2:
+                        computerAnimationImage.setImageResource(R.drawable.com_bo);
+                        if (view.getId() == R.id.gaweButton) {
+                            voicePlay("유저 승리");
+
+                        } else if (view.getId() == R.id.boButton) {
+                            voicePlay("무승부");
+
+                        } else {
+                            voicePlay("컴퓨터 승리");
+
+                        }
+                        break;
+                    case 3:
+                        computerAnimationImage.setImageResource(R.drawable.com_bawe);
+                        if (view.getId() == R.id.gaweButton) {
+                            voicePlay("컴퓨터 승리");
+
+                        } else if (view.getId() == R.id.boButton) {
+                            voicePlay("유저 승리");
+
+                        } else {
+                            voicePlay("무승부");
+
+                        }
+                        break;
+                }
+
+
                 break;
+
         }
+    }
+
+    //speech 리소스 해제
+    @Override
+    protected void onStop() {
+        super.onStop();
+        textToSpeech.shutdown();
+    }
+
+    public void voicePlay(String voiceText) {
+        textToSpeech.speak(voiceText, TextToSpeech.QUEUE_FLUSH, null, null);
+
     }
 }
